cd3e7b99e9476304be3792664ea29c5d6ea3bb11
Lukas Forst <lukas.forst@blindspot.ai>
2018-05-09 18:24:02 +0200

    now it is possible to start it

--- hw7/src/main/java/dbs/gui/MainScreen.java
+++ hw7/src/main/java/dbs/gui/MainScreen.java
@@ -6,18 +6,47 @@ import dbs.db.providers.UserProvider;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class MainScreen extends JFrame {
     private JPanel panel1;
-    private JTabbedPane tabbedPane1;
+    private JTextArea commandResponse;
+    private JTextField commandField;
+    private JTabbedPane tabbedPane;
 
     private final CharacterProvider characterProvider;
     private final FightProvider fightProvider;
     private final UserProvider userProvider;
 
+    public MainScreen() {
+        this.characterProvider = null;
+        this.fightProvider = null;
+        this.userProvider = null;
+    }
+
     public MainScreen(CharacterProvider characterProvider, FightProvider fightProvider, UserProvider userProvider) throws HeadlessException {
+        super("MainScreen");
+        super.setContentPane(panel1);
+        super.pack();
         this.characterProvider = characterProvider;
         this.fightProvider = fightProvider;
         this.userProvider = userProvider;
+
+        setUpListeners();
+    }
+
+    private void setUpListeners() {
+        commandField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                //enter
+                if (e.getKeyCode() == 10) {
+                    commandResponse.append(commandField.getText() + "\n");
+                    commandField.setText("");
+                }
+                super.keyPressed(e);
+            }
+        });
     }
 }
